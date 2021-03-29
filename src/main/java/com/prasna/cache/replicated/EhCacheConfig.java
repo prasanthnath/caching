fb2989018d27d0d8a6d153b5e543b3d7356cc9e9
Prasanth Gullapalli <gullapal@arcesium.com>
2021-03-29 14:09:31 +0530

    Package names changed

--- src/main/java/com/prasna/cache/replicated/EhCacheConfig.java
+++ src/main/java/com/prasna/cache/replicated/EhCacheConfig.java
@@ -11,7 +11,7 @@ import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
  */
 class EhCacheConfig {
     Configuration build(){
-        String ehCacheJGroupsPort= System.getenv("arc.eh_jgroups_port");
+        String ehCacheJGroupsPort= System.getenv("prasna.eh_jgroups_port");
         FactoryConfiguration factoryConfiguration = new FactoryConfiguration()
                 .className("net.sf.ehcache.distribution.jgroups.JGroupsCacheManagerPeerProviderFactory")
                 .properties("connect=TCP(bind_addr=localhost;bind_port="+ehCacheJGroupsPort+"):TCPPING(initial_hosts=localhost[7801],localhost[7802],localhost[7803];port_range=1;timeout=5000;num_initial_members=3):MERGE2(min_interval=3000;max_interval=5000):FD_ALL(interval=5000;timeout=20000):FD(timeout=5000;max_tries=48;):VERIFY_SUSPECT(timeout=1500):pbcast.NAKACK(retransmit_timeout=100,200,300,600,1200,2400,4800;discard_delivered_msgs=true):pbcast.STABLE(stability_delay=1000;desired_avg_gossip=20000;max_bytes=0):pbcast.GMS(print_local_addr=true;join_timeout=5000)")
